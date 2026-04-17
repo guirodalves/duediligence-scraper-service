9a36425f6a4bbd108f2b4e4a0cd81438fe76d640
guirodalves <guirodalves@gmail.com>
2026-04-17 10:54:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "ok"}
+    return {"message": "API OK"}
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.post("/collect")
+def collect():
+    return {"status": "test ok"}
